4c5e56f296c64b4ff43652182942e9d8b5fe07fc
duyvu <vuthanhduy93@gmail.com>
2021-07-29 16:51:28 +0700

    prototype method

--- class.js
+++ class.js
@@ -1,15 +1,30 @@
 //named class
-class rectangle{
+// class rectangle{
+//     constructor(width, height){
+//         this.width = width
+//         this.height = height
+//     }
+// }
+// //unnamed class
+// console.log(rectangle .name)
+// let rectangle2 = class {
+//     constructor(height,width){
+//         this.height=height
+//         this.width=width
+//     }
+// }
+//prototype method
+class rectangle {
     constructor(width, height){
         this.width = width
         this.height = height
     }
-}
-//unnamed class
-console.log(rectangle .name)
-let rectangle2 = class {
-    constructor(height,width){
-        this.height=height
-        this.width=width
+    get area(){
+        return this.calArea()
+    }
+    calArea(){
+        return this.width*this.height
     }
 }
+var square = new rectangle(3,5)
+console.log(square.area)
