f21a8d46fb9cbdb9b7c7b2fe24e2561a92ca8bc3
duyvu <vuthanhduy93@gmail.com>
2021-07-29 09:45:34 +0700

    day3: add revert function

--- array.js
+++ array.js
@@ -1,37 +1,42 @@
-var arr = [6,8,3,1]
-var sortArr = arr.sort(function(a,b){return a-b})
-console.log(sortArr)
+// var arr = [6,8,3,1]
+// var sortArr = arr.sort(function(a,b){return a-b})
+// console.log(sortArr)
 // var x = sortArr.length;
 // var largestNumber = sortArr[x]
 // console.log(largestNumber)
 // var total = arr.reduce(function(a,b){
-//     if(b >= a ){return a}
+//     if(b >= a ){return a}var arr = [6,8,3,1]
+// var sortArr = arr.sort(function(a,b){return a-b})
+// console.log(sortArr)
 //     else {return b}
 // },0)
 // console.log(total)
 //try catch fianlly
-function Cat (weight){
-    this.weight=weight;
-}
-var Tom = new Cat(5)
-function catwWeight(){
-    try{alertsr('this is error')}
-    catch(err){
-        console.log ('I made this error')
-    }
-}
-catwWeight()
-var s = 'agdfg'
+// function Cat (weight){
+//     this.weight=weight;
+// }
+// var Tom = new Cat(5)
+// function catwWeight(){
+//     try{alertsr('this is error')}
+//     catch(err){
+//         console.log ('I made this error')
+//     }
+// }
+// catwWeight()
+// var s = 'agdfg'
 //fisrt letter is a => return A
-function checkLetter(s){
-    let letter;
-    if ('aeiuo'.includes(s[0])){
-        return letter = 'A'
-    }
-}
-checkLetter(s)
-var splitTest = s.split('a')
-console.log(splitTest)
-var names = 'naruto, sasuke, itachi'
-var newName = names.split(',')
-console.log(newName)
+// function checkLetter(s){
+//     let letter;
+//     if ('aeiuo'.includes(s[0])){
+//         return letter = 'A'
+//     }
+// }
+// checkLetter(s)
+// var splitTest = s.split('a')
+// console.log(splitTest)
+// var names = 'naruto, sasuke, itachi'
+// var newName = names.split(',')
+// console.log(newName)
+var str = '123456'
+var reverseStr= str.split('').reverse().join('')
+console.log(reverseStr)
