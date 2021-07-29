f65f96c74d905cfe67c98f2558c828e726e4ab7a
duyvu <vuthanhduy93@gmail.com>
2021-07-29 16:41:35 +0700

    class expression

---
+++ class.js
@@ -0,0 +1,15 @@
+//named class
+class rectangle{
+    constructor(width, height){
+        this.width = width
+        this.height = height
+    }
+}
+//unnamed class
+console.log(rectangle .name)
+let rectangle2 = class {
+    constructor(height,width){
+        this.height=height
+        this.width=width
+    }
+}
